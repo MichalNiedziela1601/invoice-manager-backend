9d9f14aaebfd4bdb5df27e4ee0f343275429e329
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-03-15 13:07:25 +0100

    ITA-107 Backend / Get invoice details by id

--- app/dao/invoice.dao.js
+++ app/dao/invoice.dao.js
@@ -31,6 +31,24 @@ function addInvoice(invoice)
             })
 }
 
+function getInvoiceById(id)
+{
+    let query = 'select i.id, i.invoice_nr, i.type, i.create_date, i.execution_end_date, i.netto_value, i.brutto_value, i.status, i.url,'
+            + 'cd.id as dealer_id, cd.name as dealer_name, cd.nip as delaer_nip, cd.regon as dealer_regon, ad.street as dealer_street, '
+            + 'ad.build_nr as dealer_build_nr, ad.flat_nr as dealer_flat_nr, ad.post_code as dealer_post_code, ad.city as dealer_city,'
+            + ' cr.id as recipent_id, cr.name as recipent_name, cr.nip as recipent_nip, cr.regon as recipent_regon, ar.street as recipent_street,'
+            + ' ar.build_nr as recipent_build_nr, ar.flat_nr as recipent_falt_nr, ar.post_code as recipent_post_code, ar.city as recipent_city '
+            + ' from invoice AS i LEFT JOIN company AS cd ON i.company_dealer = cd.id LEFT JOIN company AS cr ON i.company_recipent = cr.id '
+            + 'LEFT JOIN address AS ad ON cd.address = ad.id LEFT JOIN address AS ar ON cr.address = ar.id WHERE i.id = $1;';
+    return db.oneOrNone(query, [id]).then(result =>
+    {
+        if (result === null) {
+            result = 'Invoice not found';
+        }
+        return result;
+    })
+}
+
 module.exports = {
-    getInvoice, addInvoice
+    getInvoice, addInvoice, getInvoiceById
 };
