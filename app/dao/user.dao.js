d4bd0ea13c6dd2aa4a391c83100bf8ef53eae952
Bielbiel <Bartlomiej.Biel@labnoratory.com>
2017-03-22 12:51:57 +0100

    ITA-156 Backend/ remove register auth.dao and split functionality to user.dao and company

--- app/dao/user.dao.js
+++ app/dao/user.dao.js
@@ -9,7 +9,23 @@ function getUserByEmail(email)
     })
 }
 
+function addUser(person)
+{
+    let user = 'INSERT INTO users (email, password,company_id) values ($1,$2,$3)';
+
+    return db.none(user, [person.email, person.password, data.id]).then(() =>
+    {
+
+    }).catch(error =>
+    {
+        db.none('DELETE FROM company WHERE id = $1', [data.id]);
+        console.error('ERROR auth.dao.registerCompany:', error.message || error);
+        throw error;
+    });
+}
+
 
 module.exports = {
-    getUserByEmail
+    getUserByEmail,
+    addUser
 };
